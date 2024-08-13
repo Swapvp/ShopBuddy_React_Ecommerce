254e2e897c9100ab04d116fd163a97d8b05555fd
Swapvp <swapneshpawar1994@gmail.com>
2024-08-13 11:08:08 +0530

    footer-email-conetxt

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Popular from "../Components/Popular/Popular";
 import Offers from "../Components/Offers/Offers";
+import NewCollection from "../Components/NewCollection/NewCollection";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
 
 const Shop = () => {
   return (
@@ -9,6 +11,8 @@ const Shop = () => {
       <Hero />
       <Popular />
       <Offers />
+      <NewCollection />
+      <NewsLetter />
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ShopContextProvider from "./Context/ShopContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <ShopContextProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+    ,
+  </ShopContextProvider>
+);
