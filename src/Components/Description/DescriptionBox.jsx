8511b277034bb58174a143c769680f11a115cd1d
Swapvp <swapneshpawar1994@gmail.com>
2024-08-22 17:27:02 +0530

    description

---
+++ src/Components/Description/DescriptionBox.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionBox">
+      <div className="DB-navigator">
+        <div className="DB-nav-box">Description</div>
+        <div className="DB-nav-box fade">Reviews(122)</div>
+      </div>
+      <div className="DB-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          possimus porro sunt commodi ipsam doloribus ea sed vel laudantium
+          minus dolores ratione, architecto aliquid unde, ab excepturi similique
+          exercitationem neque! Sit omnis possimus eveniet totam dignissimos
+          dolorem ullam nam reprehenderit enim at laudantium repellendus ducimus
+          nihil a, nobis eaque magnam blanditiis eum similique rerum. Iste fugit
+          ullam commodi consectetur perferendis? Facilis quo quod qui ad
+          repellendus sapiente non ullam soluta! Cum corporis labore cupiditate
+          adipisci dolores rerum fuga nisi non iste, voluptatibus nostrum,
+          molestiae perspiciatis autem error nulla in ea.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
